bf0f456275d16620031dbddc702ddeddb9bec319
LlamaLord11 <colebob.jordan@gmail.com>
2026-09-04 20:37:56 -0600

    Rebuilt versioning system to account for variable sized tables

--- database/databaseManager.py
+++ database/databaseManager.py
@@ -100,11 +100,11 @@ class Database:
 
     def version_control_writer(self, version_description: str, version_charsize: int) -> int:
         """Writes the basic version_control table entry and returns the current internal version index. 
-        All code uses this index for versioning, the alternative display_version is purely for clients/public viewing and has no bearing on code"""
+        All code uses this index for versioning, the alternative display_version is purely for clients/public viewing and has no internal use.
+        The display version is a decimal that increments by 0.1 for each new version, starting at 0"""
         lastVersion = self.readLatestDisplayVersion()
 
-        if not lastVersion:
-            lastVersion = '0.0'
+        if not lastVersion: lastVersion = '0.0'
 
         currentVersion = decimal.Decimal(lastVersion) + decimal.Decimal('0.1')
         formattedDecimal = str(currentVersion)
@@ -143,36 +143,60 @@ class Database:
 # CHANGELOG FORMATTING: internal_id (Changelog key), version_id (version_control table index link), content_id (sub table id (classes/items/spells)), content_type (item/spell/class), change_type (ADD/EDIT/DELETE), contents
 # - CONTENTS CONT: contents is a list that will contain all columns in order of each table excluding the content_id. The first item will always be the name.
 
+    def formatChangelogRow(self, currentRow: list, rowNameList: list) -> dict:
+            outputDict = {}
+            for i in range(0,len(currentRow) - 1):
+                outputDict[rowNameList[i]] = currentRow[i]
+
+            outputDict[rowNameList[-1]] = json.loads(currentRow[-1])
+    
+            return outputDict
+
     def readSingleVersion(self, version: int) -> list:
         """Returns a LIST of all changelog entries pertaining to the provided version.
-        \nFORMAT: [ internal_id (autoincriment SQL index), version_id, content_type (item/class), content_id (specific entry ID), change_type (ADD/EDIT/DELETE), contents ]"""
+
+        FORMAT: { internal_id: internal_id (autoincriment SQL index), version_id: version_id, 
+        content_id: content_id (specific entry ID), content_type: content_type (item/class/spell), 
+        change_type: change_type (ADD/EDIT/DELETE), contents: contents (another dictionary containing all rows not explicitly listed) }"""
+
+        base_info = self.cursor.execute('PRAGMA table_info(changelog)')
+        columnNames = [col[1] for col in base_info]
+
         self.cursor.execute('SELECT * FROM changelog WHERE version_id = ?', (version,))
         versionList = self.cursor.fetchall()
-
-        return versionList
-
-    def versionListFormatter(self, versionList):
-        """Outputs a list of lists. Each internal list represents 1 entry
-        \nENTRY FORMATTING: change_type (ADD + / EDIT > / DELETE -), content_type (item i / class c), content_id, content"""
-
-        change_type_hash = {
-            "ADD":"+",
-            "EDIT":'>',
-            "DELETE":"-"
-        }
-
-        content_type_hash = {
-            "item":"i",
-            "class":"c",
-            "spell":"s"
-        }
-
-        formatedList = []
+        formattedVersionList = []
 
         for row in versionList:
-            formatedList.append([change_type_hash[row[4]], content_type_hash[row[2]], row[3], json.loads(row[5])])
+            formattedVersionList.append(self.formatChangelogRow(row, columnNames))
 
-        return json.dumps(formatedList, separators=(',', ':'))
+        return formattedVersionList
+
+    # def versionListFormatterOld(self, versionList):
+    #     """Outputs a list of lists. Each internal list represents 1 entry
+    #     \nENTRY FORMATTING: change_type (ADD + / EDIT > / DELETE -), content_type (item i / class c), content_id, content"""
+
+    #     change_type_hash = {
+    #         "ADD":"+",
+    #         "EDIT":'>',
+    #         "DELETE":"-"
+    #     }
+
+    #     content_type_hash = {
+    #         "item":"i",
+    #         "class":"c",
+    #         "spell":"s"
+    #     }
+
+    #     formatedList = []
+
+    #     for row in versionList:
+    #         formatedList.append([change_type_hash[row[4]], content_type_hash[row[2]], row[3], json.loads(row[5])])
+
+    #     return json.dumps(formatedList, separators=(',', ':'))
+
+    def jsonStringMaker(self, inputDictionary):
+        # No need to hash ADD/EDIT/DELETE/Table type. At scale, it is a negligable data size reduction
+        return json.dumps(inputDictionary, separators=(',', ':'))
 
         
 
@@ -185,7 +209,7 @@ class Database:
 
         for (versionID,) in listOfVersions:
             currentVersion = self.readSingleVersion(versionID)
-            formattedVersion = self.versionListFormatter(currentVersion)
+            formattedVersion = self.jsonStringMaker(currentVersion)
             versionCache[versionID] = formattedVersion
 
         return versionCache
@@ -193,6 +217,20 @@ class Database:
 
     def close(self):
         self.connection.close()
+
+"""
+Quick planning section on the data formatting
+Changelog contents section may as well just be a formatted json string as it needs to contain a dictonary of things which in just text would
+loose all formatting. In json string format, it can easily be reverted back to a dictionary without me needing to build my own text format system
+to recognize what represents what as well as how to seperate each dictionary item, etc...
+
+What that means is that I need a function that takes in a dictionary and turns it into a json string.
+
+This is all neccesary as my changelong system needs to be able to accept a variable number of columns in the contents section as each internal table
+can vary in column count. ex: class has 3 columns while spells and items have 4. This will get more drastic as time passes and I begin to rewrite the
+database structure to actually handle the data they need to hold. The current iteration is effectively just a placeholder as I don't know what the
+final data going in will look like. Better to make it flexible now to avoid unneccesary server/backend rewrites.
+"""
 
 db = Database()
 db.databaseWriter("dummystring", ['dummyList'])
